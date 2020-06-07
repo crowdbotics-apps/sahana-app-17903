1446a93056e12d397df7cac71841a82b42f59646
Crowdbotics <team@crowdbotics.com>
2020-06-07 00:15:40 +0000

    Update models

--- backend/sahana_app_17903/wsgi.py
+++ backend/sahana_app_17903/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sahana_app_17903.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sahana_app_17903.settings")
 
 application = get_wsgi_application()
